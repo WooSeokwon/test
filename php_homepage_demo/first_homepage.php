01c88b498ebcf906131a1a867e68bc409e9e9d64
WooSeokwon <pplesb2015@gmail.com>
2017-01-12 09:40:27 +0900

    upload file

---
+++ php_homepage_demo/first_homepage.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="description" content="The liberary website's main page"/>
+    <meta name="author" content="pplesb2015@gmail.com"/>
+    <link rel="stylesheet" type="text/css" href="http://localhost/real_cording/style.css"/> <!-- css 추가-->
+    </head>
+  <body id="topic_index" class="course view sidebar everystudy logged_out revision">
+    <div id="whole_wrapper">
+      <div class="head">
+        <header>
+            <hgroup>
+              <nav id="left_nav">
+                <li><a href="#">FirstHomepage.org</a></li>
+                <li id="list"><a href="#">전체 목록</a></li>
+              </nav >
+              <nav id="right_nav">
+                <li><a href="#">로그인</a></li>
+                <li><a href="#">도움말</a></li>
+              </nav>
+              <p class="title">
+                 <a href="http://localhost/real_cording/first_homepage.php">First Homepage</a>
+              </p>
+              <p class="subtitle">
+                <small>Hello Everybody!!</small>
+              </p>
+            </hgroup>
+        </header>
+      </div>
+
+      <div class="body">
+        <div id="breadcrumb">
+          <div class="wrapper">
+            <span id="breadcrumb_line">제목없음</span>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+
+<div class="menu">
+  <nav id="topmenu">
+    <ul>
+      <li><a class="menuLink" href="#">첫번째 메뉴</a></li>
+      <li><a class="menuLink" href="#">두번째 메뉴</a></li>
+      <li><a class="menuLink" href="#">세번째 메뉴</a></li>
+      <li><a class="menuLink" href="#">네번쨰 메뉴</a></li>
+    </ul>
+  </nav>
+</div>
+
+
+
+  </body>
+</html>
